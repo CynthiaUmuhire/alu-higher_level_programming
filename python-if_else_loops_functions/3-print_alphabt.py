f551b393803ef7e111592f620d8514e993c536a9
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-10-25 15:31:53 +0200

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for letters in range(ord('a'), ord('z')+1):
-  if letters == 'e' or letters == 'q':
-    continue
- else:
-  print("{:c}" .format(letters), end =" ")
+for letter_code in range(ord('a'), ord('z')+1):
+    letter = chr(letter_code)
+    if letter not in "qe":
+        print(letter, end="")
