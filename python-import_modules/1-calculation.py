ff6f7d285bd81f7f1b937e3ee61b5e99fdf83216
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-10-28 10:34:26 +0200

    Update 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
+if __name__ = "__main__":
+  from calculator_1 import add, sub, mul, div
   a = 10
   b = 5
-  return(a + b)
-
-def sub(a, b):
-  a = 10
-  b = 5
-  return(a - b)
-
-def mul(a, b):
-  a = 10
-  b = 5
-  return(a * b)
-
-def div(a, b):
-  a = 10
-  b = 5
-  return(a / b) 
-
 print("{} + {} =  {} ".format(a, b,add(a, b)))
 print("{} - {} =  {} ".format(a, b,sub(a, b)))
 print("{} * {} =  {} ".format(a, b,mul(a, b)))
