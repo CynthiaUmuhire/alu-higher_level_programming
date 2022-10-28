7c1ad8018ba8acb902e10761d7212c340e60b676
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-10-28 11:32:09 +0200

    Update 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
   from sys import argv
-num = len(argv) - 1  
+num = len(argv) - 1
+values = 1
 if num > 0:
     if num == 1:
         print("{} argument:".format(num))
+        print("{} : {}".format(values, argv[1])
     else:
         print("{} arguments:".format(num))
-        values = 1
+        
         for arg in argv[1:]:
             print("{}: {}".format(values, arg))
             values += 1
