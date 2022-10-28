4b4d86ed00305d64772b42717022556c691e2026
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-10-28 12:03:34 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-  from sys import argv
+    from sys import argv
 num = len(argv) - 1
 sum = 0
 if num == 0:
-  print(sum)
+    print(sum)
 else:
   for i in argv[1:]:
     sum = sum + int(i)
-   
-  print(sum)
+
+    print(sum)
