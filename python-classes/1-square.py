ab05c7dff9e00833cebe76e541f84941c12713cc
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-11-10 11:56:59 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"" Creates a square ""
+
+class Square:
+  ''' A square class
+  with a private attribute: size
+  '''
+  def __init__(self, size):
+    ''' initialize size '''
+    self.size = size
