2ec8194583d2b5b389fc3da7c09f4c9e46d3f9f8
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2022-11-10 12:20:42 +0200

    Update 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Creates a square class"""
+
+class Square:
+  '''
+  creates a a square class
+       has a private instance att:size
+  '''
+  
+  def __init__(self, size=0):
+    ''' init size '''
+    if not isinstance(size, int):
+           raise TypeError("size must be an integer")
+       elif size < 0:
+           raise ValueError("size must be >= 0")
+
+        self.__size = size
