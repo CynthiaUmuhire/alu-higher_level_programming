7ffbee25660060f8a4f26b6e4cbfa3094c5a5e26
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-02-06 15:25:19 +0200

    error correction

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,15 +2,13 @@
 function add(a,b) {
     return (a+b);
 }
-let arg=prompt("Enter any argument: ");
-let a1=parseInt(arg[0]);
-let a2=parseInt(arg[1]);
-if (arg===null || arg===""){
-    console.log("No argument");
-}
-else if (a1,a2===undefined || isNaN(a1,a2)){
-    console.log("One of the arguments is not a nbr");
-}
-else {
+const arg = process.argv.slice(2);
+let a1 = parseInt(arg[0]);
+let a2 = parseInt(arg[1]);
+if (arg === null || arg === "") {
+    console.log('No argument');
+} else if (a1,a2 === undefined || isNaN(a1,a2)) {
+    console.log('One of the arguments is not a nbr');
+} else {
     console.log(add(a1,a2));
    } 
