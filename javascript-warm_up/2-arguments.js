de7b13e7bc3f0f233522c85b4fa80640a5f89f06
Cynthia <109437062+CynthiaUmuhire@users.noreply.github.com>
2023-02-06 14:47:45 +0200

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-let arg= process.argv.slice(2);
-if (arg.length===0){
-    console.log("No argument");
+const arg = process.argv.slice(2);
+if (arg.length === 0) 
+{
+  console.log("No argument");
 }
-else if(arg.length===1){
-    console.log("Argument found");
+else if (arg.length===1)
+{
+  console.log("Argument found");
 }
-else{
-    console.log("Arguments found");
+else
+{
+  console.log("Arguments found");
 }
